c2649d9dc97f7b5c72e51a3d705207c55236bc49
bhavinshah15 <shah.bhavin.girish@gmail.com>
2025-02-26 21:34:48 +0530

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -1,1 +1,17 @@
-print("My test")
+from Package1 import my_math
+import unittest
+
+class TestMyMath(unittest):
+    def setup(self):
+        print("Inside TestMyMath : setup()")
+        self.a=10
+        self.b=5
+
+    def tearDown(self):
+        print("Inside TestMyMath : tearDown()")
+        self.a=None
+        self.b=None
+
+    def test_add_positive():
+        print("Inside TestMyMath : test_add_positive()")
+        self.assertEqual(my_add(self.a,self.b), 15)
