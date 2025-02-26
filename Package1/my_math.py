bdcf8f5de50b704b5436a356ecccb0179e471865
bhavinshah15 <shah.bhavin.girish@gmail.com>
2025-02-26 22:02:12 +0530

    Update my_math.py

--- Package1/my_math.py
+++ Package1/my_math.py
@@ -3,7 +3,12 @@ print("Inside myMath.py")
 def my_add(x,y):
     return x+y
 
+'''
 def my_divide(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero")
+    return x / y
+'''
+
+def my_divide(x, y):
     return x / y
