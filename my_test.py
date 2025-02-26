d583cbdd6a4056605f5581afb4ad592402abae69
bhavinshah15 <shah.bhavin.girish@gmail.com>
2025-02-26 21:42:15 +0530

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -8,6 +8,7 @@ class TestMyMath(unittest.TestCase):
         self.b=5
 
     def tearDown(self):
+        print self.a
         print("Inside TestMyMath : tearDown()")
         self.a=None
         self.b=None
